e8407d56fe3c4f561f35677d6542e46a3ea4e724
Rafael Caricio <rafael.jacinto@gmail.com>
2011-09-05 17:57:18 -0300

    Tagging version.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(name='pygeoip',
-      version='0.1.4',
+      version='0.1.4m',
       description='Pure Python GeoIP API',
       author='Jennifer Ennis',
       author_email='zaylea@gmail.com',
